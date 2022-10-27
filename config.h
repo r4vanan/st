4643d99dbcdc6ee6cf589e7f51d67895709e82aa
Vinoth Kumar <35825039+r4vanan@users.noreply.github.com>
2022-10-27 13:10:29 +0530

    Update config.h

--- config.h
+++ config.h
@@ -5,7 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JoyPixels:pixelsize=11:antialias=true:autohint=true"};
+static char *font = "JoyPixels:pixelsize=11:antialias=true:autohint=true";
+static char *font2[] = { "JetBrains Mono :pixelsize=14:antialias=true:autohint=true" };
 static int borderpx = 0;
 
 /*
