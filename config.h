26ea5b6885e2a8e3fad07e74d5693ec6c5e1491d
r4vanan <vinothlawrence66@gmail.com>
2022-10-31 21:01:08 +0530

    updated xresources

--- config.h
+++ config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrains Mono :pixelsize=14:antialias=true:autohint=true";
-static char *font2[] = { "JetBrains Mono :pixelsize=14:antialias=true:autohint=true" };
+static char *font = "mono:pixelsize=13:antialias=true:autohint=true";
+static char *font2[] = { "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true" };
 static int borderpx = 0;
 
 /*
